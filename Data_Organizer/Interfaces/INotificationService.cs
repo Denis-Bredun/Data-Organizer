96be628f67464a5c612c170b6300845b3bff8c72
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-14 02:44:44 +0200

    Made NotificationService

---
+++ Data_Organizer/Interfaces/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Data_Organizer.Interfaces
+{
+    public interface INotificationService
+    {
+        Task ShowToastAsync(string message);
+        Task<bool> ShowConfirmationDialogAsync(string message);
+        Task<string> ShowActionSheetAsync(params string[] actions);
+    }
+}
---
+++ Data_Organizer/Services/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using Data_Organizer.Interfaces;
+
+namespace Data_Organizer.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public async Task<string> ShowActionSheetAsync(params string[] actions)
+        {
+            var currentPage = Application.Current?.MainPage;
+            return await currentPage.DisplayActionSheet("Що ви бажаєте зробити?", "Нічого", null, actions);
+        }
+
+        public async Task<bool> ShowConfirmationDialogAsync(string message)
+        {
+            var currentPage = Application.Current?.MainPage;
+            return await currentPage.DisplayAlert("Підтвердження", message, "Так", "Ні");
+        }
+
+        public async Task ShowToastAsync(string message)
+        {
+            var notification = Toast.Make(message, ToastDuration.Long);
+            await notification.Show();
+        }
+    }
+}
