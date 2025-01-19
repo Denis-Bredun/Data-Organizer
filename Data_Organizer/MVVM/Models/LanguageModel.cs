3b8cad2721b0a3ef062c47915b8e829d429b1bfd
denis.bredun <bredun.denis@gmail.com>
2025-01-19 03:03:48 +0200

    Added LanguageModel

---
+++ Data_Organizer/MVVM/Models/LanguageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Organizer.MVVM.Models
+{
+    public class LanguageModel
+    {
+        public string DisplayName { get; set; }
+        public string CultureCode { get; set; }
+    }
+}
