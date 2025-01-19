faf73e4f1afb6ac4ca56f8dd96c4f35576469e1e
denis.bredun <bredun.denis@gmail.com>
2025-01-19 03:06:37 +0200

    Added CultureInfoService

---
+++ Data_Organizer/Interfaces/ICultureInfoService.cs
@@ -0,0 +1,15 @@
+﻿using Data_Organizer.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Organizer.Interfaces
+{
+    public interface ICultureInfoService
+    {
+        CultureInfo GetCultureInfoFromLanguage(LanguageModel language);
+    }
+}
---
+++ Data_Organizer/Services/CultureInfoService.cs
@@ -0,0 +1,26 @@
+﻿using Data_Organizer.Interfaces;
+using Data_Organizer.MVVM.Models;
+using System.Globalization;
+
+namespace Data_Organizer.Services
+{
+    public class CultureInfoService : ICultureInfoService
+    {
+        public List<LanguageModel> Languages { get; }
+
+        public CultureInfoService()
+        {
+            Languages = new List<LanguageModel>
+        {
+            new LanguageModel { DisplayName = "Українська", CultureCode = "uk-UA" },
+            new LanguageModel { DisplayName = "Англійська", CultureCode = "en-US" },
+            new LanguageModel { DisplayName = "Російська", CultureCode = "ru-RU" }
+        };
+        }
+
+        public CultureInfo GetCultureInfoFromLanguage(LanguageModel language)
+        {
+            return CultureInfo.GetCultureInfo(language.CultureCode);
+        }
+    }
+}
