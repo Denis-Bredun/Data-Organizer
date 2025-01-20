2136db9f5e7e9bf7ed34610ca8dc5d186d5333a7
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-20 12:53:26 +0200

    Added MenuItemModel

---
+++ Data_Organizer/MVVM/Models/MenuItemModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Data_Organizer.MVVM.Models
+{
+    public class MenuItemModel
+    {
+        public string Title { get; set; }
+        public ImageSource? Icon { get; private set; }
+
+        public void SetIcon(string imagePath)
+        {
+            Icon = ImageSource.FromFile(imagePath);
+        }
+
+        public void RemoveIcon()
+        {
+            Icon = null;
+        }
+    }
+}
