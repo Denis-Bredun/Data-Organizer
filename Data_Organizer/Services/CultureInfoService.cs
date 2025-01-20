a089f9f7b5ca2565c35627e3363e63694bdf2e4a
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-20 12:42:41 +0200

    Changes values for DisplayName property in LanguageModel objects in CultureInfoService.

--- Data_Organizer/Services/CultureInfoService.cs
+++ Data_Organizer/Services/CultureInfoService.cs
@@ -11,11 +11,11 @@ namespace Data_Organizer.Services
         public CultureInfoService()
         {
             Languages = new List<LanguageModel>
-        {
-            new LanguageModel { DisplayName = "Українська", CultureCode = "uk-UA" },
-            new LanguageModel { DisplayName = "Англійська", CultureCode = "en-US" },
-            new LanguageModel { DisplayName = "Російська", CultureCode = "ru-RU" }
-        };
+            {
+                new LanguageModel { DisplayName = "UA", CultureCode = "uk-UA" },
+                new LanguageModel { DisplayName = "EN", CultureCode = "en-US" },
+                new LanguageModel { DisplayName = "RU", CultureCode = "ru-RU" }
+            };
         }
 
         public CultureInfo GetCultureInfoFromLanguage(LanguageModel language)
