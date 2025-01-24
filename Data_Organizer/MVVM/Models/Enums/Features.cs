683ce7645a4ed18f0298910cd60dbfb1f7221bf7
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-24 08:30:52 +0200

    Added enum for saving the names of the features.

---
+++ Data_Organizer/MVVM/Models/Enums/Features.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Data_Organizer.MVVM.Models.Enums
+{
+    public enum Features
+    {
+        [Description("Транскрипція")]
+        Transcription,
+        [Description("Конспект")]
+        Summary
+    }
+}
