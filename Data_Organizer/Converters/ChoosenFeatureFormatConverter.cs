db4d830d789306bc69297bf5ec9c59c113579f0e
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-26 01:52:33 +0200

    Customized Dropdown component for Features output. Added ChoosenFeatureFormatConverter

---
+++ Data_Organizer/Converters/ChoosenFeatureFormatConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Data_Organizer.Converters
+{
+    class ChoosenFeatureFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value} ▼";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
