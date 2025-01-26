8c9a582dce7b9f0a42575d26004d9b5d4c2e38a0
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-26 07:28:28 +0200

    Set styles for popup in Dropdown component

--- Data_Organizer/Platforms/Android/MainActivity.cs
+++ Data_Organizer/Platforms/Android/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace Data_Organizer
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/MyAppTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
