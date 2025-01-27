44981523355f99111684f912aa01dc491ca2039b
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-27 12:01:18 +0200

    Deleted using NotificationService from ClipboardService

--- Data_Organizer/Services/ClipboardService.cs
+++ Data_Organizer/Services/ClipboardService.cs
@@ -4,17 +4,9 @@ namespace Data_Organizer.Services
 {
     public class ClipboardService : IClipboardService
     {
-        private readonly INotificationService _notificationService;
-
-        public ClipboardService(INotificationService notificationService)
-        {
-            _notificationService = notificationService;
-        }
-
         public async Task Add(string text)
         {
             await Clipboard.SetTextAsync(text);
-            await _notificationService.ShowToastAsync("Дані успішно скопійовані!");
         }
 
         public async Task<string?> GetLastData()
