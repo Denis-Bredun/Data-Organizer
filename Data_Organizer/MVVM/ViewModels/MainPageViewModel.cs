804b6ae1bc9e9896c574dc4ed3857e2594b12010
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2025-01-27 13:54:40 +0200

    Finished Edit Mode Switch

--- Data_Organizer/MVVM/ViewModels/MainPageViewModel.cs
+++ Data_Organizer/MVVM/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Data_Organizer.Interfaces;
 using Data_Organizer.MVVM.Models;
 
@@ -17,6 +18,8 @@ namespace Data_Organizer.MVVM.ViewModels
         private string _outputText;
         [ObservableProperty]
         private bool _isReadOnly;
+        [ObservableProperty]
+        private string _editButtonImageSource;
 
         public IFeatureService FeatureService { get; }
         public ICultureInfoService CultureInfoService { get; }
@@ -36,7 +39,54 @@ namespace Data_Organizer.MVVM.ViewModels
             _notificationService = notificationService;
             _clipboardService = clipboardService;
 
+            SetDefaultProperties();
+        }
+
+        private void SetDefaultProperties()
+        {
             IsReadOnly = true;
+            EditButtonImageSource = "disabled_edit_mode.png";
+        }
+
+        [RelayCommand]
+        public async Task SwitchEditMode()
+        {
+            bool wasEditModeEnabled = false;
+
+            if (IsReadOnly)
+                wasEditModeEnabled = await EnableEditMode();
+            else
+                await DisableEditMode();
+
+            SwitchEditButtonImage(wasEditModeEnabled);
+        }
+
+        private void SwitchEditButtonImage(bool wasEditModeEnabled)
+        {
+            if (wasEditModeEnabled)
+                EditButtonImageSource = "enabled_edit_mode.png";
+            else
+                EditButtonImageSource = "disabled_edit_mode.png";
+        }
+
+        public async Task<bool> EnableEditMode()
+        {
+            var isConfirmed = await _notificationService.ShowConfirmationDialogAsync("Ви дійсно бажаєте увійти в режим редагування?");
+
+            if (!isConfirmed)
+                return false;
+
+            IsReadOnly = false;
+
+            await _notificationService.ShowToastAsync("Ви увійшли в режим редагування!");
+            return true;
+        }
+
+        public async Task DisableEditMode()
+        {
+            IsReadOnly = true;
+
+            await _notificationService.ShowToastAsync("Ви вийшли з режиму редагування!");
         }
     }
 }
