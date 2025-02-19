fd7e5f42123985521679e6c711aeb28c6c7172a1
Monkey D Luffy <bredun.denis@gmail.com>
2025-02-20 00:41:19 +0200

    Added default properties and commands for SignUp and SignIn ViewModels

--- Data_Organizer/MVVM/ViewModels/SignInViewModel.cs
+++ Data_Organizer/MVVM/ViewModels/SignInViewModel.cs
@@ -1,8 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Data_Organizer.MVVM.ViewModels
 {
     public partial class SignInViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string _email;
+        [ObservableProperty]
+        private string _password;
+
+        [RelayCommand]
+        public async Task NavigateToSignUpPage()
+        {
+            await Shell.Current.GoToAsync("//SignUpPage");
+        }
+
+        [RelayCommand]
+        public async Task NavigateToMainPage()
+        {
+            await Shell.Current.GoToAsync("//TabBar");
+        }
     }
 }
--- Data_Organizer/MVVM/ViewModels/SignUpViewModel.cs
+++ Data_Organizer/MVVM/ViewModels/SignUpViewModel.cs
@@ -1,8 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Data_Organizer.MVVM.ViewModels
 {
     public partial class SignUpViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string _username;
+        [ObservableProperty]
+        private string _email;
+        [ObservableProperty]
+        private string _password;
+        [ObservableProperty]
+        private string _confirmPassword;
+
+        [RelayCommand]
+        public async Task NavigateToSignInPage()
+        {
+            await Shell.Current.GoToAsync("//SignInPage");
+        }
+
+        [RelayCommand]
+        public async Task NavigateToMainPage()
+        {
+            await Shell.Current.GoToAsync("//TabBar");
+        }
     }
 }
