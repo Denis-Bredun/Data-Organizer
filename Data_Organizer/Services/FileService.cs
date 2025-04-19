d1e4009bceb0ed934a4179ad0b3021f070b97442
Monkey D Luffy <bredun.denis@gmail.com>
2025-04-19 03:18:15 +0300

    Fixed constructer in FileService

--- Data_Organizer/Services/FileService.cs
+++ Data_Organizer/Services/FileService.cs
@@ -17,9 +17,9 @@ namespace Data_Organizer.Services
     {
         private readonly ITranskriptorQueries _transkriptorQueries;
 
-        public FileService(ITranskriptorQueries _transkriptorQueries)
+        public FileService(ITranskriptorQueries transkriptorQueries)
         {
-            _transkriptorQueries = _transkriptorQueries;
+            _transkriptorQueries = transkriptorQueries;
         }
 
         public async Task<bool> RequestPermissionsStorageReadAsync()
