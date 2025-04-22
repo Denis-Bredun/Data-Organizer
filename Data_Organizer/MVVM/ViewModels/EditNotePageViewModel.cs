8dd509dadb7b93dfbd59bf6dac43e134f9cccb8d
Monkey D Luffy <bredun.denis@gmail.com>
2025-04-23 00:22:04 +0300

    Finished view for EditNotePage

--- Data_Organizer/MVVM/ViewModels/EditNotePageViewModel.cs
+++ Data_Organizer/MVVM/ViewModels/EditNotePageViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Data_Organizer.MVVM.Models;
 
 namespace Data_Organizer.MVVM.ViewModels
 {
     public partial class EditNotePageViewModel : ObservableObject
     {
         [ObservableProperty] private bool _isLoading;
+        [ObservableProperty] private NoteHeader _header;
+        [ObservableProperty] private NoteBody _body;
+
+        public EditNotePageViewModel()
+        {
+            Header = new NoteHeader();
+            Body = new NoteBody();
+        }
     }
 }
